16503df5ad599f45bfdbcf5c8ac8481277d59ce1
Nicolai-Astrup-Wiik <143996469+Nicolai-Astrup-Wiik@users.noreply.github.com>
2024-02-07 13:42:12 +0100

    fix ui to design spces

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const recipesButton = document.querySelector('.recipes-button');
+const formButton = document.querySelector('.form-button')
 const recipeForm = document.querySelector('.recipe-form-page-container');
+const recipeList = document.querySelector('.recipe-list-container');
 
 console.log(recipesButton)
 
@@ -10,4 +12,16 @@ recipesButton.addEventListener("click", () => {
 const displayRecipes = () => {
     console.log('button clicked');
 	 recipeForm.classList.remove('visible')
+	 recipeList.classList.add('visible')
+};
+
+
+formButton.addEventListener("click", () => {
+	displayForm();
+});
+
+const displayForm = () => {
+	console.log('button clicked');
+	recipeList.classList.remove('visible')
+	recipeForm.classList.add('visible')
 };
